5920837ddf48242c164be97c3f4fed09c9a9a840
junqing.fjq <junqing.fjq@alibaba-inc.com>
2016-06-28 23:04:55 +0800

    delete lib

--- lib/build/lib/goforit/pkg_info.py
+++
@@ -1,8 +0,0 @@
-package = "goforit"
-version = "1.0.0"
-license = "goforit License"
-short_description = "Command line tool for goforit."
-author = "junqing.fjq"
-long_description = """
-You can use the package to finish your work.
-"""
